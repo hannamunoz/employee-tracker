a1dba4bdcbec005e9a76a1ab61bdb20dd2a5c09c
Hanna Munoz <hannamunoz@gmail.com>
2021-03-12 13:51:57 -0500

    writing app file, adding functions for queries

--- app.js
+++ app.js
@@ -66,5 +66,39 @@ function startPrompt() {
                 addDepartment();
             break;
         }
-    })
+    });
 }
+
+// View all employees
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function(err, res) {
+        if (err) throw err
+        console.table(res);
+        startPrompt();
+    });
+}
+
+// View all roles
+function viewAllByRole() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
+    function(err, res) {
+        if (err) throw err
+        console.table(res);
+        startPrompt();
+    });
+}
+
+// View all by department
+function viewAllByDepartment() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",
+    function(err, res) {
+        if (err) throw err
+        console.table(res);
+        startPrompt();
+    });
+}
+
+// Role query and role title for adding employee
+var rolesArr = [];
+function 
